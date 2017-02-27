72f649dc7c8c289b3beb93e276dcd644f0a1b9f7
maxkram <max.kramer@live.com>
2017-02-27 22:23:06 +0300

    my 1st node

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("home");
+});
+
+app.post("/addfriend", function(req, res){
+	res.send("You have reached post route");
+});
+
+app.get("/friends", function(req, res){
+	var friends = ["Kolya", "Olya", "Vanya", "Manya", "Ramzan"];
+	res.render("friends", {friends: friends});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("server started!!!!");
+});
